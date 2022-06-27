1dd824d80fa753e3d9c7b53429e8da462b3ebe98
CompactLethargy13 <107340414+CompactLethargy13@users.noreply.github.com>
2022-06-27 17:20:00 +0800

    Add files via upload

---
+++ pog factorie/scripts/main.js
@@ -0,0 +1,25 @@
+Events.on(ClientLoadEvent, b  => {
+Blocks.graphitePress.craftTime = 1;
+Blocks.multiPress.outputItem = ItemStack(Items.graphite, 5);
+Blocks.multiPress.craftTime = 1;
+Blocks.siliconSmelter.craftTime = 1;
+Blocks.siliconSmelter.consumes.items(ItemStack.with(Items.coal, 0.5, Items.sand, 0.5));
+Blocks.siliconCrucible.craftTime = 5;
+Blocks.siliconCrucible.consumes.items(ItemStack.with(Items.coal, 1, Items.sand, 2, Items.pyratite, 1));
+Blocks.kiln.craftTime = 2;
+Blocks.plastaniumCompressor.craftTime=3;
+Blocks.plastaniumCompressor.consumes.liquid(Liquids.oil, 0.2);
+Blocks.phaseWeaver.craftTime = 4;
+Blocks.phaseWeaver.consumes.items(ItemStack.with(Items.thorium, 1, Items.sand, 1));
+Blocks.surgeSmelter.craftTime = 5;
+Blocks.surgeSmelter.consumes.items(ItemStack.with(Items.copper, 1, Items.lead, 2, Items.titanium, 1, Items.silicon, 1));
+Blocks.cryofluidMixer.craftTime = 6;
+Blocks.cryofluidMixer.consumes.liquid(Liquids.water, 0.1);
+Blocks.pyratiteMixer.consumes.items(ItemStack.with(Items.coal, 1, Items.lead, 1, Items.sand, 1));
+Blocks.separator.craftTime=1;
+Blocks.separator.consumes.liquid(Liquids.slag, 2 / 30);
+Blocks.disassembler.consumes.liquid(Liquids.slag, 0.1);
+Blocks.sporePress.outputLiquid = new LiquidStack(Liquids.oil, 20);
+Blocks.pulverizer.outputItem = new ItemStack(Items.sand, 2);
+Blocks.coalCentrifuge.craftTime=15;
+});
